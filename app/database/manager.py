7a072f2fdd665845365e04eca10fe55c2a887c19
Yannaev <jae@ur-spk.ru>
2026-02-09 15:13:25 +0500

    add DBManager

---
+++ app/database/manager.py
@@ -0,0 +1,17 @@
+class DBManager:
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        # self.users = UsersRepository(self.session)
+
+        return self
+
+    async def __aexit__(self, *args):
+        await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
