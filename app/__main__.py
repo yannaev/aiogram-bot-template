912f7db12af96f52b97c996dbe1223a66bc300e8
Yannaev <jae@ur-spk.ru>
2026-02-09 11:54:13 +0500

    add include_routers

--- app/__main__.py
+++ app/__main__.py
@@ -5,10 +5,15 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from app.config import settings
+from app.handlers.admin import admin_router
+from app.handlers.start import start_router
 
 
 async def main() -> None:
     dp = Dispatcher()
+
+    dp.include_routers(start_router, admin_router)
+
     bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     await dp.start_polling(bot)
