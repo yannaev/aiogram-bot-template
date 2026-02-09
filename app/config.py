4f32f427a408e555aefe96c69991e987b8c61ec5
Yannaev <jae@ur-spk.ru>
2026-02-09 11:54:55 +0500

    add ADMIN_IDS

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
+    ADMIN_IDS: list[int]
 
     model_config = SettingsConfigDict(env_file=".env")
 
