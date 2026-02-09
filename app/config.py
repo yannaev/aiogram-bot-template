74588273a4f013c41c719a38c47999b42d3f53a4
Yannaev <jae@ur-spk.ru>
2026-02-09 15:05:47 +0500

    add db_url

--- app/config.py
+++ app/config.py
@@ -1,9 +1,29 @@
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
-    ADMIN_IDS: list[int]
+    bot_token: str
+    admin_ids: list[int]
+
+    db_host: str
+    db_port: int
+    db_user: str
+    db_pass: str
+    db_name: str
+
+    @property
+    def db_url(self) -> str:
+        return str(
+            MultiHostUrl.build(
+                scheme="postgresql+asyncpg",
+                username=self.db_user,
+                password=self.db_pass,
+                host=self.db_host,
+                port=self.db_port,
+                path=self.db_name,
+            )
+        )
 
     model_config = SettingsConfigDict(env_file=".env")
 
