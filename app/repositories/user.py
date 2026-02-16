53fb9fb040852143fd27bf462ac001e64486be66
Yannaev <jae@ur-spk.ru>
2026-02-16 17:51:45 +0500

    delete user create

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -6,7 +6,3 @@ from app.schemas.user import User, UserCreate
 class UserRepository(BaseRepository):
     model = UserOrm
     schema = User
-
-    async def create_user(self, telegram_id: int) -> User:
-        user = UserCreate(telegram_id=telegram_id)
-        return await self.create(data=user)
