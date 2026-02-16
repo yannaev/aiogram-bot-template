61eccccb905ec48f70ac4c5f251a532e63da1156
Yannaev <jae@ur-spk.ru>
2026-02-16 17:03:52 +0500

    format with ruff

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -19,17 +19,19 @@ class BaseRepository:
         self.session = session
 
     async def create(self, data: BaseModel):
-        stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        stmt = (
+            insert(self.model).values(**data.model_dump(exclude_unset=True)).returning(self.model)
+        )
 
         try:
             result = await self.session.execute(stmt)
+            model = result.scalar_one()
+            return self.schema.model_validate(model)
+
         except IntegrityError as ex:
             if isinstance(ex.orig.__cause__, UniqueViolationError):
                 raise ObjectAlreadyExistsException
             raise ex
-
-        model = result.scalar_one()
-        return self.schema.model_validate(model)
 
     async def get_filtered(self, *filters, **filter_by):
         query = select(self.model).filter(*filters).filter_by(**filter_by)
@@ -45,8 +47,10 @@ class BaseRepository:
 
         try:
             model = result.scalar_one()
+
         except NoResultFound:
             raise ObjectNotFoundException
+
         except MultipleResultsFound:
             raise MultipleObjectsFoundException
 
@@ -56,27 +60,27 @@ class BaseRepository:
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
         model = result.scalar_one_or_none()
+
         if model is None:
             return None
         return self.schema.model_validate(model)
 
     async def update(self, data: BaseModel, exclude_unset: bool = True, **filter_by):
         payload = data.model_dump(exclude_unset=exclude_unset)
+
         if not payload:
             return await self.get_one_or_none(**filter_by)
 
-        stmt = (
-            update(self.model)
-            .filter_by(**filter_by)
-            .values(**payload)
-            .returning(self.model)
-        )
+        stmt = update(self.model).filter_by(**filter_by).values(**payload).returning(self.model)
+
         try:
             result = await self.session.execute(stmt)
             model = result.scalar_one_or_none()
+
             if model is None:
                 return None
             return self.schema.model_validate(model)
+
         except IntegrityError as ex:
             if isinstance(ex.orig.__cause__, UniqueViolationError):
                 raise ObjectAlreadyExistsException
@@ -88,12 +92,16 @@ class BaseRepository:
         try:
             result = await self.session.execute(stmt)
             model = result.scalar_one_or_none()
+
             if model is None:
                 raise ObjectNotFoundException
+
         except IntegrityError as ex:
             if isinstance(ex.orig.__cause__, ForeignKeyViolationError):
                 raise ObjectInUseException from ex
             raise ex
 
-    async def delete_all(self):
-        await self.session.execute(delete(self.model))
+    async def delete_all(self) -> int:
+        stmt = delete(self.model)
+        result = await self.session.execute(stmt)
+        return result.rowcount
