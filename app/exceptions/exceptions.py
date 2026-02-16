6e2ffa1c957550769f5b66c1ba27634c5b11a484
Yannaev <jae@ur-spk.ru>
2026-02-16 14:35:35 +0500

    add ObjectAlreadyExistsException and ObjectNotFoundException

---
+++ app/exceptions/exceptions.py
@@ -0,0 +1,12 @@
+class AppException(Exception):
+    message = "Неожиданная ошибка"
+
+    def __init__(self, message: str | None = None):
+        self.message = message or self.message
+        super().__init__(self.message)
+
+class ObjectAlreadyExistsException(AppException):
+    message = "Объект уже существует"
+
+class ObjectNotFoundException(AppException):
+    message = "Объект не найден"
