0feb8c7348ff79aab6840ad076bc769e0fc9936f
Yannaev <jae@ur-spk.ru>
2026-02-16 16:57:09 +0500

    add update method

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,6 +1,6 @@
 from asyncpg import UniqueViolationError, ForeignKeyViolationError
 from pydantic import BaseModel
-from sqlalchemy import insert, select, delete
+from sqlalchemy import insert, select, delete, update
 from sqlalchemy.exc import IntegrityError, NoResultFound, MultipleResultsFound
 
 from app.exceptions.exceptions import (
@@ -59,6 +59,28 @@ class BaseRepository:
         if model is None:
             return None
         return self.schema.model_validate(model)
+
+    async def update(self, data: BaseModel, exclude_unset: bool = True, **filter_by):
+        payload = data.model_dump(exclude_unset=exclude_unset)
+        if not payload:
+            return await self.get_one_or_none(**filter_by)
+
+        stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**payload)
+            .returning(self.model)
+        )
+        try:
+            result = await self.session.execute(stmt)
+            model = result.scalar_one_or_none()
+            if model is None:
+                return None
+            return self.schema.model_validate(model)
+        except IntegrityError as ex:
+            if isinstance(ex.orig.__cause__, UniqueViolationError):
+                raise ObjectAlreadyExistsException
+            raise ex
 
     async def delete(self, **filter_by) -> None:
         stmt = delete(self.model).filter_by(**filter_by).returning(self.model)
