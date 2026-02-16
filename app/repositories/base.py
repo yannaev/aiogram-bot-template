ae34c775426c775654e882405d31afe593c3c2c5
Yannaev <jae@ur-spk.ru>
2026-02-16 14:35:53 +0500

    add ObjectAlreadyExistsException

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,5 +1,10 @@
+from asyncpg import UniqueViolationError
 from pydantic import BaseModel
 from sqlalchemy import insert
+from sqlalchemy.exc import IntegrityError
+
+from app.exceptions.exceptions import ObjectAlreadyExistsException
+
 
 class BaseRepository:
     model = None
@@ -9,8 +14,14 @@ class BaseRepository:
         self.session = session
 
     async def create(self, data: BaseModel):
-        add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
-        result = await self.session.execute(add_data_stmt)
+        stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+
+        try:
+            result = await self.session.execute(stmt)
+        except IntegrityError as ex:
+            if isinstance(ex.orig.__cause__, UniqueViolationError):
+                raise ObjectAlreadyExistsException
+            raise ex
+
         model = result.scalars().one()
         return self.schema.model_validate(model)
-
