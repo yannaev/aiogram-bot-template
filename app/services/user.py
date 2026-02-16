b5843e37e186914160c5dcea76035f68eb9b2c92
Yannaev <jae@ur-spk.ru>
2026-02-16 17:52:20 +0500

    add UserService

---
+++ app/services/user.py
@@ -0,0 +1,15 @@
+from app.exceptions.exceptions import ObjectAlreadyExistsException
+from app.schemas.user import User, UserCreate
+from app.services.base import BaseService
+
+
+class UserService(BaseService):
+    async def register_user(self, telegram_id: int) -> User:
+        user_schema = UserCreate(telegram_id=telegram_id)
+
+        try:
+            user = await self.db.user.create(data=user_schema)
+            await self.db.commit()
+            return user
+        except ObjectAlreadyExistsException:
+            return await self.db.user.get_one(telegram_id=telegram_id)
