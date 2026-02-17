4e39f80d64a3645bbb88596d178571a276b78227
Yannaev <jae@ur-spk.ru>
2026-02-17 14:19:57 +0500

    format with ruff

--- app/models/user.py
+++ app/models/user.py
@@ -14,4 +14,6 @@ class UserORM(Base):
     referrer_telegram_id: Mapped[int | None] = mapped_column(BigInteger, index=True)
     is_blocked: Mapped[bool] = mapped_column(server_default=false())
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
