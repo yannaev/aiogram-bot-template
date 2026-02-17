2170b0be80431cef56487f22732d1dfd59f4191b
Yannaev <jae@ur-spk.ru>
2026-02-17 14:18:39 +0500

    update user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,8 +5,11 @@ from app.schemas.base import BaseSchema
 
 class UserCreateDTO(BaseSchema):
     telegram_id: int
+    referrer_telegram_id: int | None = None
 
 
 class UserDTO(UserCreateDTO):
     id: int
+    is_blocked: bool
     created_at: datetime
+    updated_at: datetime
