326807bfc52e62fbfd5ba197e4fbaac6ff00c2e1
Yannaev <69728833+yannaev@users.noreply.github.com>
2026-02-20 12:19:06 +0500

    add block and unblock user

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -32,12 +32,12 @@ async def back_to_main_menu(callback: CallbackQuery, state: FSMContext) -> None:
 @admin_router.callback_query(F.data == "get_user")
 async def get_user(callback: CallbackQuery, state: FSMContext) -> None:
     await callback.answer()
-    await state.set_state(AdminStates.waiting_for_telegram_id)
+    await state.set_state(AdminStates.get_user)
     await callback.message.edit_text("Send telegram_id.", reply_markup=AdminKeyboards.back_to_menu)
 
 
-@admin_router.message(AdminStates.waiting_for_telegram_id)
-async def process_user_id(message: Message, state: FSMContext, db: DBManager) -> None:
+@admin_router.message(AdminStates.get_user)
+async def get_user_state(message: Message, state: FSMContext, db: DBManager) -> None:
     if not message.text.isdigit():
         await message.answer(
             "Only numbers are allowed. Try again.", reply_markup=AdminKeyboards.back_to_menu
@@ -51,6 +51,66 @@ async def process_user_id(message: Message, state: FSMContext, db: DBManager) ->
     if user:
         await message.answer(
             text=f"<pre><code class='language-json'>{user.model_dump_json(indent=4)}</code></pre>",
+            reply_markup=AdminKeyboards.back_to_menu,
+        )
+    else:
+        await message.answer("User not found.", reply_markup=AdminKeyboards.back_to_menu)
+
+    await state.clear()
+
+
+@admin_router.callback_query(F.data == "block_user")
+async def block_user(callback: CallbackQuery, state: FSMContext) -> None:
+    await callback.answer()
+    await state.set_state(AdminStates.block_user)
+    await callback.message.edit_text("Send telegram_id.", reply_markup=AdminKeyboards.back_to_menu)
+
+
+@admin_router.message(AdminStates.block_user)
+async def block_user_state(message: Message, state: FSMContext, db: DBManager) -> None:
+    if not message.text.isdigit():
+        await message.answer(
+            "Only numbers are allowed. Try again.", reply_markup=AdminKeyboards.back_to_menu
+        )
+        return
+
+    telegram_id = int(message.text)
+
+    user = await UserService(db).block(telegram_id=telegram_id)
+
+    if user:
+        await message.answer(
+            text=f"Success. User has been blocked.",
+            reply_markup=AdminKeyboards.back_to_menu,
+        )
+    else:
+        await message.answer("User not found.", reply_markup=AdminKeyboards.back_to_menu)
+
+    await state.clear()
+
+
+@admin_router.callback_query(F.data == "unblock_user")
+async def unblock_user(callback: CallbackQuery, state: FSMContext) -> None:
+    await callback.answer()
+    await state.set_state(AdminStates.unblock_user)
+    await callback.message.edit_text("Send telegram_id.", reply_markup=AdminKeyboards.back_to_menu)
+
+
+@admin_router.message(AdminStates.unblock_user)
+async def unblock_user_state(message: Message, state: FSMContext, db: DBManager) -> None:
+    if not message.text.isdigit():
+        await message.answer(
+            "Only numbers are allowed. Try again.", reply_markup=AdminKeyboards.back_to_menu
+        )
+        return
+
+    telegram_id = int(message.text)
+
+    user = await UserService(db).unblock(telegram_id=telegram_id)
+
+    if user:
+        await message.answer(
+            text=f"Success. User has been unblocked.",
             reply_markup=AdminKeyboards.back_to_menu,
         )
     else:
--- app/states/admin.py
+++ app/states/admin.py
@@ -2,4 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class AdminStates(StatesGroup):
-    waiting_for_telegram_id = State()
+    get_user = State()
+    block_user = State()
+    unblock_user = State()
